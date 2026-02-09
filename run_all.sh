20d20fbc22afdffd7a3e74ea5c7ac6be16ff21a4
Xia, Weiwen <weiwen.xia@intel.com>
2026-02-09 13:41:19 +0800

    Wait for 60s before handling next repo; add sglang report manually

--- run_all.sh
+++ run_all.sh
@@ -1,8 +1,8 @@
 python main.py pytorch pytorch
-sleep 20
+sleep 60
 python main.py pytorch ao
-sleep 20
+sleep 60
 python main.py sgl-project sglang
-sleep 20
+sleep 60
 python main.py vllm-project vllm
 
